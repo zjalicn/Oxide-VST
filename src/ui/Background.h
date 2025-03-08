e2c84cd94d50f92480024976c76a8a93a8a9a410
Nikola Zjalic <nzjalic@gmail.com>
2025-03-08 11:49:27 -0500

    change meters placement

--- src/ui/Background.h
+++ src/ui/Background.h
@@ -22,6 +22,13 @@ public:
             g.drawImageWithin(backgroundImage, 0, 0, getWidth(), getHeight(), 
                              juce::RectanglePlacement::stretchToFit);
         }
+        else
+        {
+            // Debug text to show if image loading failed
+            g.setColour(juce::Colours::red);
+            g.setFont(14.0f);
+            g.drawText("Background image failed to load", getLocalBounds(), juce::Justification::centred);
+        }
     }
 
 private:
@@ -29,47 +36,82 @@ private:
 
     void loadBackgroundImage()
     {
-        // Get the current executable directory
-        juce::File exeFile = juce::File::getSpecialLocation(juce::File::currentApplicationFile);
-        juce::File exeDir = exeFile.getParentDirectory();
+        juce::Logger::writeToLog("Attempting to load background image...");
         
-        // Check several potential locations for the Resources folder
-        juce::Array<juce::File> possibleResourceDirs;
+        // 1. Try the binary resource
+        int size = 0;
+        const char* data = BinaryData::getNamedResource("bg_png", size);
         
-        // 1. Resources in the same directory as the executable
-        possibleResourceDirs.add(exeDir.getChildFile("Resources"));
-        
-        // 2. Resources in the parent directory
-        possibleResourceDirs.add(exeDir.getParentDirectory().getChildFile("Resources"));
-        
-        // 3. Current working directory
-        possibleResourceDirs.add(juce::File::getCurrentWorkingDirectory().getChildFile("Resources"));
-        
-        // 4. One level up from current directory
-        possibleResourceDirs.add(juce::File::getCurrentWorkingDirectory().getParentDirectory().getChildFile("Resources"));
-        
-        // Try each location
-        for (const auto& resourceDir : possibleResourceDirs)
+        if (data != nullptr && size > 0)
         {
-            if (resourceDir.isDirectory())
+            juce::Logger::writeToLog("Found binary resource bg_png with size: " + juce::String(size));
+            backgroundImage = juce::ImageCache::getFromMemory(data, size);
+            
+            if (backgroundImage.isValid())
             {
-                juce::File bgFile = resourceDir.getChildFile("bg.jpg");
-                if (bgFile.existsAsFile())
+                juce::Logger::writeToLog("Successfully loaded background from binary resource");
+                return;
+            }
+            else
+            {
+                juce::Logger::writeToLog("Binary resource found but image failed to load");
+            }
+        }
+        else
+        {
+            juce::Logger::writeToLog("Binary resource bg_png not found");
+        }
+        
+        // 2. Try direct file loading
+        juce::File sourceDir = juce::File::getCurrentWorkingDirectory().getChildFile("src/resources");
+        juce::Logger::writeToLog("Looking for image in: " + sourceDir.getFullPathName());
+        
+        juce::File pngFile = sourceDir.getChildFile("bg.png");
+        juce::Logger::writeToLog("Full path to PNG: " + pngFile.getFullPathName());
+        
+        if (pngFile.existsAsFile())
+        {
+            juce::Logger::writeToLog("PNG file exists with size: " + juce::String(pngFile.getSize()));
+            backgroundImage = juce::ImageCache::getFromFile(pngFile);
+            
+            if (backgroundImage.isValid())
+            {
+                juce::Logger::writeToLog("Successfully loaded background from file system");
+            }
+            else
+            {
+                juce::Logger::writeToLog("File exists but image failed to load - possibly corrupt or invalid format");
+            }
+        }
+        else
+        {
+            juce::Logger::writeToLog("PNG file not found at path: " + pngFile.getFullPathName());
+            
+            // 3. Try some alternative paths
+            juce::Array<juce::File> possiblePaths;
+            possiblePaths.add(juce::File::getCurrentWorkingDirectory().getChildFile("build/src/resources/bg.png"));
+            possiblePaths.add(juce::File::getSpecialLocation(juce::File::currentApplicationFile).getParentDirectory().getChildFile("resources/bg.png"));
+            possiblePaths.add(juce::File::getSpecialLocation(juce::File::currentExecutableFile).getParentDirectory().getChildFile("resources/bg.png"));
+            
+            for (const auto& path : possiblePaths)
+            {
+                juce::Logger::writeToLog("Trying alternate path: " + path.getFullPathName());
+                if (path.existsAsFile())
                 {
-                    backgroundImage = juce::ImageFileFormat::loadFrom(bgFile);
+                    juce::Logger::writeToLog("Found PNG at alternate path");
+                    backgroundImage = juce::ImageCache::getFromFile(path);
+                    
                     if (backgroundImage.isValid())
+                    {
+                        juce::Logger::writeToLog("Successfully loaded from alternate path");
                         return;
+                    }
                 }
             }
         }
         
-        // If we get here, we couldn't find the image file
-        // Check if it's in BinaryData
-        int size = 0;
-        if (auto* data = BinaryData::getNamedResource("bg_jpg", size))
-        {
-            backgroundImage = juce::ImageFileFormat::loadFrom(data, size);
-        }
+        // If we got here, we failed to load the image
+        juce::Logger::writeToLog("All attempts to load background image failed");
     }
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Background)
